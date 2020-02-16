596e3283fd9d4194a025868d63ad9f8d0efad3ab
DonHitYep <44013616+DonHitYep@users.noreply.github.com>
2020-02-16 22:03:30 +0800

    Update
    Update

---
+++ Assets/CosmosFramework/Base/Actor/Actor.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Cosmos
+{
+    /// <summary>
+    /// 所有可操控角色的基类
+    /// </summary>
+    public abstract class Actor : MonoBehaviour
+    {
+    }
+}
