4ebedccec1b59a40eef81d4ac34e10e6cfcb6f77
DonHitYep <zhengdonn@gmail.com>
2020-10-08 15:11:06 +0800

    Update：新增MessagePack协议的支持；

---
+++ Assets/CosmosFramework/Core/Base/Utility/Helper/MessagePackHelper/IMessagePackHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cosmos
+{
+    public interface IMessagePackHelper
+    {
+        string ToJson<T>(T obj);
+        byte[] ToByteArray<T>(T obj);
+        T ToObject<T>(byte[] buffer);
+        object ToObject(byte[] buffer , Type objectType);
+    }
+}
