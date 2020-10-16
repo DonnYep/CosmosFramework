2d193e43419692b2c8783937939fccab363104b7
Don <zhengdonn@gmail.com>
2020-10-16 13:29:08 +0800

    Bugfix：修复注释bug；

--- Assets/CosmosFramework/EditorExtension/Editor/AnnotationScriptProcessor/AnnotationScriptProcessor.cs
+++ Assets/CosmosFramework/EditorExtension/Editor/AnnotationScriptProcessor/AnnotationScriptProcessor.cs
@@ -34,10 +34,14 @@ namespace Cosmos.CosmosEditor
                 string path = name.Replace(".meta", "");
                 if (path.EndsWith(".cs"))
                 {
-                    providerAnnoTitle = providerAnnoTitle.Replace("#CreateTime#", System.DateTime.Now.ToString("yyy-MM-dd HH:ss"));
-                    providerAnnoTitle = providerAnnoTitle.Replace("#Author#", EditorConst.AnnotationAuthor);
-                    providerAnnoTitle += File.ReadAllText(path);
-                    File.WriteAllText(path, providerAnnoTitle);
+                    var str = File.ReadAllText(path);
+                    if (!str.Contains(providerAnnoTitle))
+                    {
+                        str = providerAnnoTitle + str;
+                    }
+                    str = str.Replace("#CreateTime#", System.DateTime.Now.ToString("yyy-MM-dd HH:ss"));
+                    str = str.Replace("#Author#", EditorConst.AnnotationAuthor);
+                    File.WriteAllText(path, str);
                 }
             }
             else
@@ -45,10 +49,14 @@ namespace Cosmos.CosmosEditor
                 string path = name.Replace(".meta", "");
                 if (path.EndsWith(".cs"))
                 {
-                    annotationStr = annotationStr.Replace("#CreateTime#", System.DateTime.Now.ToString("yyy-MM-dd HH:ss"));
-                    annotationStr = annotationStr.Replace("#Author#", EditorConst.AnnotationAuthor);
-                    annotationStr += File.ReadAllText(path);
-                    File.WriteAllText(path, annotationStr);
+                    var str = File.ReadAllText(path);
+                    if (!str.Contains(annotationStr))
+                    {
+                        str = annotationStr + str;
+                    }
+                    str = str.Replace("#CreateTime#", System.DateTime.Now.ToString("yyy-MM-dd HH:ss"));
+                    str = str.Replace("#Author#", EditorConst.AnnotationAuthor);
+                    File.WriteAllText(path, str);
                 }
             }
         }
