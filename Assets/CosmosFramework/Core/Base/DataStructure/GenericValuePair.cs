1a4a52020462b323d88772669b7d198abc3bea63
Don <zhengdonn@gmail.com>
2021-03-30 15:15:15 +0800

    Optimized：优化"泛型组合结构体“；

--- Assets/CosmosFramework/Core/Base/DataStructure/GenericValuePair.cs
+++ Assets/CosmosFramework/Core/Base/DataStructure/GenericValuePair.cs
@@ -6,23 +6,29 @@ using System.Threading.Tasks;
 
 namespace Cosmos
 {
-    public struct GenericPair<TValue1, TValue2> : IEquatable<GenericPair<TValue1, TValue2>>
+    /// <summary>
+    /// 双泛型值组合；此组合并非Key-Value形式，而是Value-Value形式。
+    /// 比较时调用Equals方法；进行组合时注意覆写Equals；
+    /// </summary>
+    /// <typeparam name="TValue1">泛型类型</typeparam>
+    /// <typeparam name="TValue2">泛型类型</typeparam>
+    public struct GenericValuePair<TValue1, TValue2> : IEquatable<GenericValuePair<TValue1, TValue2>>
     {
         TValue1 value1;
         TValue2 value2;
         public TValue1 Value1 { get { return value1; } }
         public TValue2 Value2 { get { return value2; } }
 
-        public GenericPair(TValue1 value1, TValue2 value2)
+        public GenericValuePair(TValue1 value1, TValue2 value2)
         {
             this.value1 = value1;
             this.value2 = value2;
         }
-        public static bool operator ==(GenericPair<TValue1, TValue2> a, GenericPair<TValue1, TValue2> b)
+        public static bool operator ==(GenericValuePair<TValue1, TValue2> a, GenericValuePair<TValue1, TValue2> b)
         {
             return a.Equals(b);
         }
-        public static bool operator !=(GenericPair<TValue1, TValue2> a, GenericPair<TValue1, TValue2> b)
+        public static bool operator !=(GenericValuePair<TValue1, TValue2> a, GenericValuePair<TValue1, TValue2> b)
         {
             return !a.Equals(b);
         }
@@ -31,7 +37,7 @@ namespace Cosmos
         /// </summary>
         public override bool Equals(object obj)
         {
-            return obj is GenericPair<TValue1, TValue2> && Equals((GenericPair<TValue1, TValue2>)obj);
+            return obj is GenericValuePair<TValue1, TValue2> && Equals((GenericValuePair<TValue1, TValue2>)obj);
         }
         public override int GetHashCode()
         {
@@ -48,7 +54,7 @@ namespace Cosmos
         /// <summary>
         /// Equals方法需要注意，若在对象未覆写Equals时，值类型比较值是否相同，引用类型比较地址是否相同；
         /// </summary>
-        public bool Equals(GenericPair<TValue1, TValue2> other)
+        public bool Equals(GenericValuePair<TValue1, TValue2> other)
         {
             bool result = false;
             if (this.GetType() == other.GetType())
