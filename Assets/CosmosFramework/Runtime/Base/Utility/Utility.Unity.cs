ae7cb8de3a44e0329857523edeafa90268675b15
DonHitYep <zhengdonn@gmail.com>
2021-12-20 23:01:06 +0800

    Update：新增unity工具函数；

--- Assets/CosmosFramework/Runtime/Base/Utility/Utility.Unity.cs
+++ Assets/CosmosFramework/Runtime/Base/Utility/Utility.Unity.cs
@@ -9,6 +9,7 @@ using System.IO;
 using System.Collections;
 using UnityEngine.Networking;
 using UnityEngine.Video;
+using UnityEngine.UI;
 
 namespace Cosmos
 {
@@ -303,6 +304,170 @@ where T : Component
             {
                 return path.Contains("\\") || path.Contains("/");
             }
+
+            /// <summary>
+            /// 角度转向量 
+            /// </summary>
+            public static Vector2 AngleToVector2D(float angle)
+            {
+                float radian = Mathf.Deg2Rad * angle;
+                return new Vector2(Mathf.Cos(radian), Mathf.Sin(radian)).normalized;
+            }
+
+            /// <summary>
+            /// 返回两个向量的夹角
+            /// </summary>
+            public static float VectorAngle(Vector2 from, Vector2 to)
+            {
+                float angle;
+                Vector3 cross = Vector3.Cross(from, to);
+                angle = Vector2.Angle(from, to);
+                return cross.z > 0 ? -angle : angle;
+            }
+            /// <summary>
+            /// 双线性插值法缩放图片，等比缩放 
+            /// </summary>
+            public static Texture2D ScaleTextureBilinear(Texture2D originalTexture, float scaleFactor)
+            {
+                Texture2D newTexture = new Texture2D(Mathf.CeilToInt(originalTexture.width * scaleFactor), Mathf.CeilToInt(originalTexture.height * scaleFactor));
+                float scale = 1.0f / scaleFactor;
+                int maxX = originalTexture.width - 1;
+                int maxY = originalTexture.height - 1;
+                for (int y = 0; y < newTexture.height; y++)
+                {
+                    for (int x = 0; x < newTexture.width; x++)
+                    {
+                        float targetX = x * scale;
+                        float targetY = y * scale;
+                        int x1 = Mathf.Min(maxX, Mathf.FloorToInt(targetX));
+                        int y1 = Mathf.Min(maxY, Mathf.FloorToInt(targetY));
+                        int x2 = Mathf.Min(maxX, x1 + 1);
+                        int y2 = Mathf.Min(maxY, y1 + 1);
+
+                        float u = targetX - x1;
+                        float v = targetY - y1;
+                        float w1 = (1 - u) * (1 - v);
+                        float w2 = u * (1 - v);
+                        float w3 = (1 - u) * v;
+                        float w4 = u * v;
+                        Color color1 = originalTexture.GetPixel(x1, y1);
+                        Color color2 = originalTexture.GetPixel(x2, y1);
+                        Color color3 = originalTexture.GetPixel(x1, y2);
+                        Color color4 = originalTexture.GetPixel(x2, y2);
+                        Color color = new Color(Mathf.Clamp01(color1.r * w1 + color2.r * w2 + color3.r * w3 + color4.r * w4),
+                            Mathf.Clamp01(color1.g * w1 + color2.g * w2 + color3.g * w3 + color4.g * w4),
+                            Mathf.Clamp01(color1.b * w1 + color2.b * w2 + color3.b * w3 + color4.b * w4),
+                            Mathf.Clamp01(color1.a * w1 + color2.a * w2 + color3.a * w3 + color4.a * w4)
+                        );
+                        newTexture.SetPixel(x, y, color);
+
+                    }
+                }
+                newTexture.Apply();
+                return newTexture;
+            }
+
+            /// <summary> 
+            /// 双线性插值法缩放图片为指定尺寸 
+            /// </summary>
+            public static Texture2D SizeTextureBilinear(Texture2D originalTexture, Vector2 size)
+            {
+                Texture2D newTexture = new Texture2D(Mathf.CeilToInt(size.x), Mathf.CeilToInt(size.y));
+                float scaleX = originalTexture.width / size.x;
+                float scaleY = originalTexture.height / size.y;
+                int maxX = originalTexture.width - 1;
+                int maxY = originalTexture.height - 1;
+                for (int y = 0; y < newTexture.height; y++)
+                {
+                    for (int x = 0; x < newTexture.width; x++)
+                    {
+                        float targetX = x * scaleX;
+                        float targetY = y * scaleY;
+                        int x1 = Mathf.Min(maxX, Mathf.FloorToInt(targetX));
+                        int y1 = Mathf.Min(maxY, Mathf.FloorToInt(targetY));
+                        int x2 = Mathf.Min(maxX, x1 + 1);
+                        int y2 = Mathf.Min(maxY, y1 + 1);
+
+                        float u = targetX - x1;
+                        float v = targetY - y1;
+                        float w1 = (1 - u) * (1 - v);
+                        float w2 = u * (1 - v);
+                        float w3 = (1 - u) * v;
+                        float w4 = u * v;
+                        Color color1 = originalTexture.GetPixel(x1, y1);
+                        Color color2 = originalTexture.GetPixel(x2, y1);
+                        Color color3 = originalTexture.GetPixel(x1, y2);
+                        Color color4 = originalTexture.GetPixel(x2, y2);
+                        Color color = new Color(Mathf.Clamp01(color1.r * w1 + color2.r * w2 + color3.r * w3 + color4.r * w4),
+                            Mathf.Clamp01(color1.g * w1 + color2.g * w2 + color3.g * w3 + color4.g * w4),
+                            Mathf.Clamp01(color1.b * w1 + color2.b * w2 + color3.b * w3 + color4.b * w4),
+                            Mathf.Clamp01(color1.a * w1 + color2.a * w2 + color3.a * w3 + color4.a * w4)
+                        );
+                        newTexture.SetPixel(x, y, color);
+
+                    }
+                }
+                newTexture.Apply();
+                return newTexture;
+            }
+            /// <summary> 
+            /// Texture旋转
+            /// </summary>
+            public static Texture2D RotateTexture(Texture2D texture, float eulerAngles)
+            {
+                int x;
+                int y;
+                int i;
+                int j;
+                float phi = eulerAngles / (180 / Mathf.PI);
+                float sn = Mathf.Sin(phi);
+                float cs = Mathf.Cos(phi);
+                Color32[] arr = texture.GetPixels32();
+                Color32[] arr2 = new Color32[arr.Length];
+                int W = texture.width;
+                int H = texture.height;
+                int xc = W / 2;
+                int yc = H / 2;
+
+                for (j = 0; j < H; j++)
+                {
+                    for (i = 0; i < W; i++)
+                    {
+                        arr2[j * W + i] = new Color32(0, 0, 0, 0);
+
+                        x = (int)(cs * (i - xc) + sn * (j - yc) + xc);
+                        y = (int)(-sn * (i - xc) + cs * (j - yc) + yc);
+
+                        if ((x > -1) && (x < W) && (y > -1) && (y < H))
+                        {
+                            arr2[j * W + i] = arr[y * W + x];
+                        }
+                    }
+                }
+
+                Texture2D newImg = new Texture2D(W, H);
+                newImg.SetPixels32(arr2);
+                newImg.Apply();
+
+                return newImg;
+            }
+
+            /// <summary> 
+            /// 在指定物体上添加指定图片 
+            /// </summary>
+            public static Image AddImage(GameObject target, Sprite sprite)
+            {
+                target.SetActive(false);
+                Image image = target.GetComponent<Image>();
+                if (!image)
+                    image = target.AddComponent<Image>();
+                image.sprite = sprite;
+                image.SetNativeSize();
+                target.SetActive(true);
+                return image;
+            }
+
+
 
             #region CaptureScreenshot
             /// <summary>
