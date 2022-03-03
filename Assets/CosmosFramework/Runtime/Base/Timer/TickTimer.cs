bd988d364a0030e1c9922f772bce8b668c857098
Don <zhengdonn@gmail.com>
2022-03-03 11:14:01 +0800

    Update：更新计时器；

--- Assets/CosmosFramework/Runtime/Base/Timer/TickTimer.cs
+++ Assets/CosmosFramework/Runtime/Base/Timer/TickTimer.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
+
 namespace Cosmos
 {
     /// <summary>
@@ -37,39 +39,59 @@ namespace Cosmos
                 this.CancelCallback = cancelCallback;
                 this.StartTime = startTime;
             }
+            public void Dispose()
+            {
+                this.TaskId = 0;
+                this.LoopCount = 0;
+                this.IntervalTime = 0;
+                this.DestTime = 0;
+                this.TaskCallback = null;
+                this.CancelCallback = null;
+                this.StartTime = 0;
+            }
         }
-
         public Action<string> LogInfo { get; set; }
         public Action<string> LogWarn { get; set; }
         public Action<string> LogError { get; set; }
-
         readonly DateTime startDateTime = new DateTime(1970, 1, 1, 0, 0, 0);
         readonly ConcurrentDictionary<int, TickTask> taskDict;
         readonly object locker = new object();
         int taskIndex = 0;
-
         public int TaskCount { get { return taskDict.Count; } }
+        Queue<TickTask> taskQueue;
+        bool usePool;
+        public bool UsePool { get { return usePool; } }
         /// <summary>
         /// 计时器构造函数；
         /// </summary>
-        public TickTimer()
+        /// <param name="usePool">是否使用池缓对task进行缓存。任务量大时，建议为true</param>
+        public TickTimer(bool usePool = false)
         {
             taskDict = new ConcurrentDictionary<int, TickTask>();
+            this.usePool = usePool;
+            if (usePool)
+                taskQueue = new Queue<TickTask>();
         }
         /// <summary>
         /// 添加任务；
+        /// 若任务添加成功，则返回大于0的TaskId；
+        /// 若任务添加失败，则返回-1；
         /// </summary>
         /// <param name="intervalTime">毫秒级别时间间隔</param>
         /// <param name="taskCallback">执行回调</param>
         /// <param name="cancelCallback">任务取消回调</param>
         /// <param name="loopCount">执行次数</param>
-        /// <returns></returns>
+        /// <returns>添加事件成功后返回的ID</returns>
         public int AddTask(int intervalTime, Action<int> taskCallback, Action<int> cancelCallback, int loopCount = 1)
         {
             int tid = GenerateTaskId();
             double startTime = GetUTCMilliseconds();
             double destTime = startTime + intervalTime;
-            var task = new TickTask(tid, loopCount, intervalTime, destTime, taskCallback, cancelCallback, startTime);
+            TickTask task = null;
+            if (usePool)
+                task = AcquireTickTask(tid, loopCount, intervalTime, destTime, taskCallback, cancelCallback, startTime);
+            else
+                task = new TickTask(tid, loopCount, intervalTime, destTime, taskCallback, cancelCallback, startTime);
             if (!taskDict.TryAdd(tid, task))
                 return -1;
             return tid;
@@ -84,6 +106,8 @@ namespace Cosmos
             if (!taskDict.TryRemove(taskId, out TickTask task))
                 return false;
             task.CancelCallback?.Invoke(taskId);
+            if (usePool)
+                ReleaseTickTask(task);
             return true;
         }
         /// <summary>
@@ -94,13 +118,10 @@ namespace Cosmos
             try
             {
                 double nowTime = GetUTCMilliseconds();
-                foreach (var item in taskDict)
+                foreach (var task in taskDict.Values)
                 {
-                    TickTask task = item.Value;
                     if (nowTime < task.DestTime)
-                    {
                         continue;
-                    }
                     ++task.LoopIndex;
                     //循环次数++，若循环idx比循环总数小，则进入下次循环；
                     if (task.LoopIndex < task.LoopCount)
@@ -116,6 +137,8 @@ namespace Cosmos
                         {
                             task.TaskCallback.Invoke(task.TaskId);
                             task.CancelCallback = null;
+                            if (usePool)
+                                ReleaseTickTask(task);
                         }
                     }
                 }
@@ -131,23 +154,20 @@ namespace Cosmos
         public void Reset()
         {
             taskDict.Clear();
+            if (usePool)
+                taskQueue.Clear();
         }
         int GenerateTaskId()
         {
             lock (locker)
             {
-                while (true)
+                while (taskDict.ContainsKey(taskIndex))
                 {
                     ++taskIndex;
                     if (taskIndex == int.MaxValue)
-                    {
                         taskIndex = 0;
-                    }
-                    if (!taskDict.ContainsKey(taskIndex))
-                    {
-                        return taskIndex;
-                    }
                 }
+                return taskIndex;
             }
         }
         /// <summary>
@@ -157,6 +177,29 @@ namespace Cosmos
         {
             TimeSpan ts = DateTime.UtcNow - startDateTime;
             return ts.TotalMilliseconds;
+        }
+        TickTask AcquireTickTask(int taskId, int loopCount, int intervalTime, double destTime, Action<int> taskCallback, Action<int> cancelCallback, double startTime)
+        {
+            TickTask task = null;
+            if (taskQueue.Count > 0)
+            {
+                task = taskQueue.Dequeue();
+                task.TaskId = taskId;
+                task.LoopCount = loopCount;
+                task.IntervalTime = intervalTime;
+                task.DestTime = destTime;
+                task.TaskCallback = taskCallback;
+                task.CancelCallback = cancelCallback;
+                task.StartTime = startTime;
+            }
+            else
+                task = new TickTask(taskId, loopCount, intervalTime, destTime, taskCallback, cancelCallback, startTime);
+            return task;
+        }
+        void ReleaseTickTask(TickTask task)
+        {
+            task.Dispose();
+            taskQueue.Enqueue(task);
         }
     }
 }
