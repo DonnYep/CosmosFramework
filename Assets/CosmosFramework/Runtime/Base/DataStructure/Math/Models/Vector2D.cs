067443d3a794b4b2a25db3b59e202594fb2d824a
DonHitYep <44013616+DonnYep@users.noreply.github.com>
2022-10-06 14:28:38 +0800

    Update: 新增数学类与string扩展

---
+++ Assets/CosmosFramework/Runtime/Base/DataStructure/Math/Models/Vector2D.cs
@@ -0,0 +1,18 @@
+﻿namespace Cosmos
+{
+    public class Vector2D
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Vector2D(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        public static Vector2D operator *(Vector2D vec, double factor)
+        {
+            return new Vector2D(vec.X * factor, vec.Y * factor);
+        }
+    }
+}
