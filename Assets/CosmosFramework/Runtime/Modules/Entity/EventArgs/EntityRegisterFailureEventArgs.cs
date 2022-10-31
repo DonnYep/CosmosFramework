51d308793f31bc760a576668affdf8e1502689bc
Donn <zhengdonn@gmail.com>
2022-10-31 16:33:43 +0800

    Update: 更新entity事件

---
+++ Assets/CosmosFramework/Runtime/Modules/Entity/EventArgs/EntityRegisterFailureEventArgs.cs
@@ -0,0 +1,18 @@
+﻿namespace Cosmos.Entity
+{
+    public class EntityRegisterFailureEventArgs : GameEventArgs
+    {
+        public override void Release()
+        {
+        }
+        internal static EntityRegisterFailureEventArgs Create()
+        {
+            var eventArgs = ReferencePool.Acquire<EntityRegisterFailureEventArgs>();
+            return eventArgs;
+        }
+        internal static void Release(EntityRegisterFailureEventArgs eventArgs)
+        {
+            ReferencePool.Release(eventArgs);
+        }
+    }
+}
---
+++ Assets/CosmosFramework/Runtime/Modules/Entity/EventArgs/EntityRegisterSuccessEventArgs.cs
@@ -0,0 +1,18 @@
+﻿namespace Cosmos.Entity
+{
+    public class EntityRegisterSuccessEventArgs : GameEventArgs
+    {
+        public override void Release()
+        {
+        }
+        internal static EntityRegisterSuccessEventArgs Create()
+        {
+            var eventArgs = ReferencePool.Acquire<EntityRegisterSuccessEventArgs>();
+            return eventArgs;
+        }
+        internal static void Release(EntityRegisterSuccessEventArgs eventArgs)
+        {
+            ReferencePool.Release(eventArgs);
+        }
+    }
+}
