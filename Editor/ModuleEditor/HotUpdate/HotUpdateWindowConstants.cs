43b4b5f1df9ac4cb47fcf64927e781b25b79aae3
Donn <zhengdonn@gmail.com>
2022-11-30 15:33:03 +0800

    Update: 更新upm

---
+++ Editor/ModuleEditor/HotUpdate/HotUpdateWindowConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Cosmos.Editor.Hotfix
+{
+    public class HotUpdateWindowConstants
+    {
+        public readonly static string DllExtension = ".dll";
+        public readonly static string PdbExtension = ".pdb";
+        public readonly static string AppendExtension = ".bytes";
+    }
+}
