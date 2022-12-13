87fa72c0ef34a73cb6e67e3c02055434c99b05fb
Donn <zhengdonn@gmail.com>
2022-12-13 14:58:27 +0800

    Update: 提交文件夹结构变更

---
+++ Assets/CosmosFramework/Runtime/Modules/Resource/State/ResourceBundleState.cs
@@ -0,0 +1,10 @@
+﻿namespace Cosmos.Resource.State
+{
+    /// <summary>
+    /// 资源包状态；
+    /// 用于查询引用状态，加载信息等；
+    /// </summary>
+    public struct ResourceBundleState
+    {
+    }
+}
---
+++ Assets/CosmosFramework/Runtime/Modules/Resource/State/ResourceObjectState.cs
@@ -0,0 +1,10 @@
+﻿namespace Cosmos.Resource.State
+{
+    /// <summary>
+    /// 资源对象状态；
+    /// 用于查看引用计数，加载信息等；
+    /// </summary>
+    public struct ResourceObjectState
+    {
+    }
+}
