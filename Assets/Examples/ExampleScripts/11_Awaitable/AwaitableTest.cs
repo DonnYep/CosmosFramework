4feba311104cc551b0baf3c7b02e14e201d0368d
Donn <zhengdonn@gmail.com>
2022-12-15 17:57:19 +0800

    Update: 提交线程异步案例

--- Assets/Examples/ExampleScripts/11_Awaitable/AwaitableTest.cs
+++ Assets/Examples/ExampleScripts/11_Awaitable/AwaitableTest.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using UnityEngine;
 using Cosmos;
+using System.Threading.Tasks;
+using Cosmos.Awaitable;
 public class AwaitableTest : MonoBehaviour
 {
     private async void Start()
@@ -11,6 +13,15 @@ public class AwaitableTest : MonoBehaviour
         await EnumRun();
         Debug.LogError("AwaitableTest >>> After Coroutine Start");
         await EnumWait();
+        var task = Task.Run(async
+           () =>
+           {
+               await Task.Delay(3000);
+               Debug.Log("Task.Delay(3000) run Done");
+               return 40;
+           });
+        await task.AsCoroutine();
+        Debug.Log("await Task.Delay(3000) Done");
     }
     IEnumerator EnumRun()
     {
