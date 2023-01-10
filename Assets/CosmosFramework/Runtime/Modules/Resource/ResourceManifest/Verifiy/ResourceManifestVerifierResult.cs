8a16a6db61e2130d565f8519eac2641fabad7e90
Donn <zhengdonn@gmail.com>
2023-01-10 14:00:06 +0800

    Update: 更新资源校验信息

---
+++ Assets/CosmosFramework/Runtime/Modules/Resource/ResourceManifest/Verifiy/ResourceManifestVerifierResult.cs
@@ -0,0 +1,14 @@
+﻿namespace Cosmos.Resource.Verifiy
+{
+    public class ResourceManifestVerifierResult
+    {
+        /// <summary>
+        /// 校验成功的信息；
+        /// </summary>
+        public ResourceManifestVerifiyInfo[] VerifiySuccessInfos; 
+        /// <summary>
+        /// 校验失败的信息；
+        /// </summary>
+        public ResourceManifestVerifiyInfo[] VerifiyFailureInfos; 
+    }
+}
