a2f6ff8daa154a9d3519840edf6d18a6d2752a50
Donn <44013616+DonnYep@users.noreply.github.com>
2023-01-19 22:41:02 +0800

    Update: 新增扩展方法

---
+++ Assets/CosmosFramework/Runtime/Base/Extensions/BaseType/RandomExts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Cosmos
+{
+    public static class RandomExts
+    {
+        /// <summary>
+        /// 生成真正的随机数
+        /// </summary>
+        public static int StrictNext(this Random @this, int maxValue = int.MaxValue)
+        {
+            return new Random(BitConverter.ToInt32(Guid.NewGuid().ToByteArray(), 0)).Next(maxValue);
+        }
+    }
+}
