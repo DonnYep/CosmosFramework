2a8462dd712cb97de63dcffa657d6f0f6bc81355
Donn <44013616+DonnYep@users.noreply.github.com>
2023-01-27 13:53:24 +0800

    Update: 新增基础类扩展

---
+++ Assets/CosmosFramework/Runtime/Base/Extensions/BaseType/LongExts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Cosmos
+{
+    public static class LongExts
+    {
+        /// <summary>
+        /// 转换成字节数组
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static byte[] GetBytes(this long value)
+        {
+            return BitConverter.GetBytes(value);
+        }
+    }
+}
---
+++ Assets/CosmosFramework/Runtime/Base/Extensions/BaseType/ShortExts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Cosmos
+{
+    public static class ShortExts
+    {
+        /// <summary>
+        /// 转换成字节数组
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static byte[] GetBytes(this short value)
+        {
+            return BitConverter.GetBytes(value);
+        }
+    }
+}
