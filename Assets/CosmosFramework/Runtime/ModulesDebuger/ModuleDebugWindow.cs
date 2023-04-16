d939687622727a45e139304be2a69ed646cad926
Donn <44013616+DonnYep@users.noreply.github.com>
2023-04-16 21:04:07 +0800

    Update: 调整模块扩展，准备开发module debuger

---
+++ Assets/CosmosFramework/Runtime/ModulesDebuger/ModuleDebugWindow.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// cosmos module debug window base 
+/// </summary>
+public class ModuleDebugWindow : MonoBehaviour
+{
+
+}
