c1e21ef5080fa2b5b81f524d4570f0c205f0303c
Donn <zhengdonn@gmail.com>
2023-05-12 17:53:20 +0800

    Update: 新增资源构建handler接口

---
+++ Assets/CosmosFramework/Editor/ModuleEditor/Resource/Tabs/AssetBundleTab/IResourceBuildHandler.cs
@@ -0,0 +1,9 @@
+﻿using UnityEditor;
+namespace Cosmos.Editor.Resource
+{
+    public interface IResourceBuildHandler
+    {
+        void OnBuildStart(BuildTarget buildTarget, string assetBundleBuildPath);
+        void OnBuildComplete(BuildTarget buildTarget, string assetBundleBuildPath);
+    }
+}
