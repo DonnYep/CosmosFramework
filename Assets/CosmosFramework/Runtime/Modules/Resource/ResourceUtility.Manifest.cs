ca14b148a673879bf914fb8eecc247eb1e9d5380
Donn <44013616+DonnYep@users.noreply.github.com>
2023-05-21 17:41:56 +0800

    Update: 优化资源模块工具类，修复构建ab时拷贝错误的资源的bug

---
+++ Assets/CosmosFramework/Runtime/Modules/Resource/ResourceUtility.Manifest.cs
@@ -0,0 +1,46 @@
+﻿namespace Cosmos.Resource
+{
+    public static partial class ResourceUtility
+    {
+        public static class Manifest
+        {
+            public static string Serialize(ResourceManifest manifest, string key)
+            {
+                var keyBytes = GenerateBytesAESKey(key);
+                return Serialize(manifest, keyBytes);
+            }
+            public static string Serialize(ResourceManifest manifest, byte[] keyBytes)
+            {
+                var manifestJson = Utility.Json.ToJson(manifest);
+                var hasKey = keyBytes != null && keyBytes.Length > 0;
+                string context = string.Empty;
+                if (hasKey)
+                    context = Utility.Encryption.AESEncryptStringToString(manifestJson, keyBytes);
+                else
+                    context = manifestJson;
+                return context;
+            }
+            public static ResourceManifest Deserialize(string manifestContext, string key)
+            {
+                var keyBytes = GenerateBytesAESKey(key);
+                return Deserialize(manifestContext, keyBytes);
+            }
+            public static ResourceManifest Deserialize(string manifestContext, byte[] keyBytes)
+            {
+                ResourceManifest manifest = null;
+                try
+                {
+                    var hasKey = keyBytes != null && keyBytes.Length > 0;
+                    var context = manifestContext;
+                    if (hasKey)
+                    {
+                        context = Utility.Encryption.AESDecryptStringToString(manifestContext, keyBytes);
+                    }
+                    manifest = Utility.Json.ToObject<ResourceManifest>(context);
+                }
+                catch { }
+                return manifest;
+            }
+        }
+    }
+}
---
+++ Assets/Examples/ExampleScripts/02_ResourceLoad/MyBundleLoader.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Cosmos;
+public class MyBundleLoader : MonoBehaviour
+{
+    string[] prefabNames = new string[]
+    {
+        "AudioSource","Capsule","Cube","Cylinder","Sphere","YBot_Idle","ResPrefab_Capsule","ResPrefab_Cube","ResPrefab_Sphere"
+    };
+    void Start()
+    {
+        for (int i = 0; i < prefabNames.Length; i++)
+        {
+            CosmosEntry.ResourceManager.LoadPrefabAsync(prefabNames[i], null, null, true);
+        }
+    }
+}
