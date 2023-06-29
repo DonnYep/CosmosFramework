6d869ff227c52767838df9ab13ba5fa96f80f7e3
Donn <zhengdonn@gmail.com>
2023-06-29 16:36:34 +0800

    Update: 优化fsm模块代码

---
+++ Assets/CosmosFramework/Runtime/Modules/FSM/FSMTransition.cs
@@ -0,0 +1,10 @@
+﻿namespace Cosmos.FSM
+{
+    public abstract  class FSMTransition<T>
+        where T:class
+    {
+        protected string transitionName;
+        public string TransitionName { get { return transitionName; } }
+        public abstract bool Handler(IFSM<T> fsm);
+    }
+}
--- Assets/Examples/ExampleScripts/07_FSM/Trigger/ExitTestTransition.cs
+++ Assets/Examples/ExampleScripts/07_FSM/Trigger/ExitTestTransition.cs
@@ -1,14 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Cosmos;
+﻿using UnityEngine;
 using Cosmos.FSM;
-public class EnterTestTrigger : FSMTrigger<Transform>
+public class ExitTestTransition : FSMTransition<Transform>
 {
     public override bool Handler(IFSM<Transform> fsm)
     {
         float distance = Vector3.Distance(fsm.Owner.transform.position, FSMTester.Instance.Target.position);
-        if (distance <= FSMTester.Instance.ObjectDetectionRange)
+        if (distance > FSMTester.Instance.ObjectDetectionRange)
             return true;
         else
             return false;
