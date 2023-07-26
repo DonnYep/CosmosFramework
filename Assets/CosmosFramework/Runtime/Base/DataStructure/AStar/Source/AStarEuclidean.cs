40dd5c4f3c3a8175e7246e41b8eb74d0788efd98
Donn <zhengdonn@gmail.com>
2023-07-26 14:03:23 +0800

    Update: 更新astar以及案例，修正获取临近节点算法

--- Assets/CosmosFramework/Runtime/Base/DataStructure/AStar/Source/AStarEuclidean.cs
+++ Assets/CosmosFramework/Runtime/Base/DataStructure/AStar/Source/AStarEuclidean.cs
@@ -5,7 +5,7 @@ namespace Cosmos
     public class AStarEuclidean : AStar
     {
         public AStarEuclidean(float gridCenterX, float gridCenterY, int xCount, int yCount, float nodeSideLength)
-            : base(gridCenterX, gridCenterY, xCount, yCount, nodeSideLength){}
+            : base(gridCenterX, gridCenterY, xCount, yCount, nodeSideLength) { }
         protected override int GetDistance(Node a, Node b)
         {
             return GetEuclideanDistance(a, b);
