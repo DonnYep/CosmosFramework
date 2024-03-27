13d38fc062286417a0f6eb7e63e676b9446f6468
郑佳东 <zhengdonn@gmail.com>
2024-03-27 09:12:43 +0800

    Update: 新增动画组件扩展

--- Assets/CosmosFramework/Runtime/Base/Extensions/Unity/AnimatorExts.cs
+++ Assets/CosmosFramework/Runtime/Base/Extensions/Unity/AnimatorExts.cs
@@ -15,5 +15,28 @@ namespace Cosmos
             }
             return @this.GetCurrentAnimatorStateInfo(layer).normalizedTime % 1;
         }
+        /// <summary>
+        /// 检测动画是否播放中 
+        /// </summary>
+        public static bool IsAnimationPlaying(this Animator @this, string animationName)
+        {
+            return @this.GetCurrentAnimatorStateInfo(0).IsName(animationName);
+        }
+        public static string GetCurrentClipName(this Animator @this)
+        {
+            AnimatorClipInfo[] clipInfo = @this.GetCurrentAnimatorClipInfo(0);
+            if (clipInfo.Length > 0)
+            {
+                // 获取当前播放动画片段的名称
+                string currentAnimation = clipInfo[0].clip.name;
+                return currentAnimation;
+            }
+            return string.Empty;
+        }
+        public static float CurrentNormalizedTime(this Animator @this)
+        {
+            var stateInfo = @this.GetCurrentAnimatorStateInfo(0);
+            return stateInfo.normalizedTime;
+        }
     }
 }
