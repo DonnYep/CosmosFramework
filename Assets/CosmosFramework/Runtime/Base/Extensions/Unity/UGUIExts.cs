3e51ebbce90139fdfb2bf9217f0e626431c26340
郑佳东 <zhengdonn@gmail.com>
2024-04-19 09:13:10 +0800

    update: 扩展ugui监听方法

--- Assets/CosmosFramework/Runtime/Base/Extensions/Unity/UGUIExts.cs
+++ Assets/CosmosFramework/Runtime/Base/Extensions/Unity/UGUIExts.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -8,53 +7,50 @@ namespace Cosmos
 {
     public static class UGUIExts
     {
-        public static Button AddButtonClickListener(this Button @this, UnityAction<BaseEventData> handle)
+        /// <summary>
+        /// 监听点击按下事件。
+        /// </summary>
+        /// <param name="this">扩展对象</param>
+        /// <param name="handle">监听的方法</param>
+        /// <returns>UIBehaviour </returns>
+        public static UIBehaviour AddClickDownListener(this UIBehaviour @this, UnityAction<BaseEventData> handle)
         {
-            var eventTrigger = @this.gameObject.GetOrAddComponent<EventTrigger>();
-            if (handle == null)
-                throw new ArgumentNullException(nameof(handle));
-            EventTrigger.Entry entry = eventTrigger.triggers.Find(e => e.eventID == EventTriggerType.PointerClick);
-            if (entry == null)
-            {
-                entry = new EventTrigger.Entry { eventID = EventTriggerType.PointerClick };
-                eventTrigger.triggers.Add(entry);
-            }
-            entry.callback.AddListener(handle);
-            return @this;
+            return AddEventListener(@this, EventTriggerType.PointerDown, handle);
         }
-        public static Button AddButtonDownListener(this Button @this, UnityAction<BaseEventData> handle)
+        /// <summary>
+        /// 监听点击事件。
+        /// <para>此事件的执行优先级高于 <see cref="AddClickUpListener"/></para> 
+        /// <para>UGUI的执行优先级<see cref="EventTriggerType.PointerClick"/> > <see cref="EventTriggerType.PointerUp"/></para> 
+        /// </summary>
+        /// <param name="this">扩展对象</param>
+        /// <param name="handle">监听的方法</param>
+        /// <returns>UIBehaviour </returns>
+        public static UIBehaviour AddClickListener(this UIBehaviour @this, UnityAction<BaseEventData> handle)
         {
-            var eventTrigger = @this.gameObject.GetOrAddComponent<EventTrigger>();
-            if (handle == null)
-                throw new ArgumentNullException(nameof(handle));
-            EventTrigger.Entry entry = eventTrigger.triggers.Find(e => e.eventID == EventTriggerType.PointerDown);
-            if (entry == null)
-            {
-                entry = new EventTrigger.Entry { eventID = EventTriggerType.PointerDown };
-                eventTrigger.triggers.Add(entry);
-            }
-            entry.callback.AddListener(handle);
-            return @this;
+            return AddEventListener(@this, EventTriggerType.PointerClick, handle);
         }
-        public static Button AddButtonUpListener(this Button @this, UnityAction<BaseEventData> handle)
+        /// <summary>
+        /// 监听点击抬起事件。
+        /// <para>此事件的执行优先级低于 <see cref="AddClickListener"/></para> 
+        /// <para>UGUI的执行优先级<see cref="EventTriggerType.PointerClick"/> > <see cref="EventTriggerType.PointerUp"/></para> 
+        /// </summary>
+        /// <param name="this">扩展对象</param>
+        /// <param name="handle">监听的方法</param>
+        /// <returns>UIBehaviour </returns>
+        public static UIBehaviour AddClickUpListener(this UIBehaviour @this, UnityAction<BaseEventData> handle)
         {
-            var eventTrigger = @this.gameObject.GetOrAddComponent<EventTrigger>();
-            if (handle == null)
-                throw new ArgumentNullException(nameof(handle));
-            EventTrigger.Entry entry = eventTrigger.triggers.Find(e => e.eventID == EventTriggerType.PointerUp);
-            if (entry == null)
-            {
-                entry = new EventTrigger.Entry { eventID = EventTriggerType.PointerUp };
-                eventTrigger.triggers.Add(entry);
-            }
-            entry.callback.AddListener(handle);
-            return @this;
+            return AddEventListener(@this, EventTriggerType.PointerUp, handle);
         }
-        public static Button AddListener(this Button @this, EventTriggerType triggerType, UnityAction<BaseEventData> handle)
+        /// <summary>
+        /// 监听事件
+        /// </summary>
+        /// <param name="this">扩展对象</param>
+        /// <param name="triggerType">事件类型</param>
+        /// <param name="handle">监听的方法</param>
+        /// <returns>UIBehaviour </returns>
+        public static UIBehaviour AddEventListener(this UIBehaviour @this, EventTriggerType triggerType, UnityAction<BaseEventData> handle)
         {
             var eventTrigger = @this.gameObject.GetOrAddComponent<EventTrigger>();
-            if (handle == null)
-                throw new ArgumentNullException(nameof(handle));
             EventTrigger.Entry entry = eventTrigger.triggers.Find(e => e.eventID == triggerType);
             if (entry == null)
             {
@@ -64,20 +60,28 @@ namespace Cosmos
             entry.callback.AddListener(handle);
             return @this;
         }
-        public static Button RemoveListener(this Button @this, EventTriggerType triggerType, UnityAction<BaseEventData> handle)
+        public static UIBehaviour RemoveClickDownListener(this UIBehaviour @this, UnityAction<BaseEventData> handle)
+        {
+            return RemoveEventListener(@this, EventTriggerType.PointerDown, handle);
+        }
+        public static UIBehaviour RemoveClickListener(this UIBehaviour @this, UnityAction<BaseEventData> handle)
+        {
+            return RemoveEventListener(@this, EventTriggerType.PointerClick, handle);
+        }
+        public static UIBehaviour RemoveClickUpListener(this UIBehaviour @this, UnityAction<BaseEventData> handle)
+        {
+            return RemoveEventListener(@this, EventTriggerType.PointerUp, handle);
+        }
+        public static UIBehaviour RemoveEventListener(this UIBehaviour @this, EventTriggerType triggerType, UnityAction<BaseEventData> handle)
         {
             var eventTrigger = @this.gameObject.GetOrAddComponent<EventTrigger>();
-            if (handle == null)
-                throw new ArgumentNullException(nameof(handle));
             EventTrigger.Entry entry = eventTrigger.triggers.Find(e => e.eventID == triggerType);
             entry?.callback.RemoveListener(handle);
             return @this;
         }
-        public static Button RemoveAllListeners(this Button @this, EventTriggerType triggerType)
+        public static UIBehaviour RemoveAllEventListeners(this UIBehaviour @this, EventTriggerType triggerType)
         {
             var eventTrigger = @this.GetComponent<EventTrigger>();
-            if (eventTrigger == null)
-                throw new ArgumentNullException(nameof(eventTrigger));
             EventTrigger.Entry entry = eventTrigger.triggers.Find(e => e.eventID == triggerType);
             entry?.callback.RemoveAllListeners();
             return @this;
@@ -100,7 +104,7 @@ namespace Cosmos
             }
         }
         /// <summary> 
-        /// 在指定物体上添加指定图片 
+        /// 在指定物体上添加指定图片
         /// </summary>
         public static Image AddImage(this GameObject @this, Sprite sprite)
         {
